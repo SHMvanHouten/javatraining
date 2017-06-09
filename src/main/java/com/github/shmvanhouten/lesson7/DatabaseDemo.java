a28db698192e9209efe0b9a2985a0a3c9c6e94bb
Sjoerd <svanhouten@gmail.com>
2017-06-09 09:24:42 +0200

    code for lesson 7 jdbc java database connectivity

---
+++ src/main/java/com/github/shmvanhouten/lesson7/DatabaseDemo.java
@@ -0,0 +1,7 @@
+package com.github.shmvanhouten.lesson7;
+
+/**
+ * Created by Eigenaar on 8-6-2017.
+ */
+public class DatabaseDemo {
+}
---
+++ src/test/java/com/github/shmvanhouten/lesson7/DatabaseDemoTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by Eigenaar on 8-6-2017.
+ */
+public class DatabaseDemoTest {
+
+}
