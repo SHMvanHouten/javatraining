d57fd95a3eaea407d21a8dbaf998e0ea5dbb129f
Sjoerd <svanhouten@gmail.com>
2017-06-10 13:59:06 +0200

    tried to add a hackable method but can't quite get it to work yet.

--- src/main/java/com/github/shmvanhouten/lesson7/CustomerHandler.java
+++ src/main/java/com/github/shmvanhouten/lesson7/CustomerHandler.java
@@ -88,10 +88,6 @@ public class CustomerHandler {
     }
 
     public Integer findCustomerId(String email) {
-        //unsafe way:
-//        String sql = "SELECT CustomerId FROM Customer WHERE Email = '" + email + "'";
-//        return jdbcTemplate.queryForObject(sql, Integer.class);
-        //safe way:
         String sql = "SELECT * FROM Customer WHERE Email = ?";
         String[] args = {email};
 //        return jdbcTemplate.queryForObject(sql, args, Integer.class); doesn't work..
@@ -107,7 +103,17 @@ public class CustomerHandler {
         String sql = "SELECT * From Customer WHERE Email = ?";
         String[] args = {email};
         List<Customer> customers = jdbcTemplate.query(sql, args, new CustomerRowMapper());
-        if(customers.size() >1){
+        if(customers.size() != 1){
+            return null;
+        }else{
+            return customers.get(0);
+        }
+    }
+
+    public Customer unsafeFindCustomer(String email){
+        String sql = "SELECT * FROM Customer WHERE Email = '" + email + "'";
+        List<Customer> customers = jdbcTemplate.query(sql, new CustomerRowMapper());
+        if(customers.size() != 1){
             return null;
         }else{
             return customers.get(0);
--- src/main/java/com/github/shmvanhouten/lesson7/CustomerServicePoint.java
+++ src/main/java/com/github/shmvanhouten/lesson7/CustomerServicePoint.java
@@ -11,6 +11,7 @@ public class CustomerServicePoint {
     private int userServiceChoice = 0;
     private int serviceStep = 0;
 
+    private Integer customerId;
     private String firstName;
     private String lastName;
     private String email;
@@ -31,6 +32,7 @@ public class CustomerServicePoint {
         System.out.println("Would you like to:");
         System.out.println("Register as a new customer? (enter 1)");
         System.out.println("Report an address change? (enter 2)");
+        System.out.println("hack into our system? (enter 3)");
     }
 
     private void startInputStream(CustomerHandler handler) {
@@ -50,13 +52,52 @@ public class CustomerServicePoint {
                 changeCustomerAddress(handler, inputLine);
                 inputLine = reader.readLine();
             }
+            while (inputLine != null && userServiceChoice == 3) {
+                unsafeLogin(handler, inputLine);
+                inputLine = reader.readLine();
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private void changeCustomerAddress(CustomerHandler handler, String userInput) {
+    private void unsafeLogin(CustomerHandler handler, String userInput) {
+        Customer customer = handler.unsafeFindCustomer(userInput);
+        String name = customer.getFirstName() + " " + customer.getLastName();
+        System.out.println("Welcome, " + name);
+    }
 
+    private void changeCustomerAddress(CustomerHandler handler, String userInput) {
+        switch (serviceStep) {
+            case 0:
+                Customer customer = handler.findCustomer(userInput);
+                if(customer == null){
+                    System.out.println("Email not found");
+                }else{
+                    String name = customer.getFirstName() + " " + customer.getLastName();
+                    customerId = customer.getCustomerId();
+                    System.out.println("Welcome, " + name);
+                    printQuestion("new address");
+                    serviceStep++;
+                }
+                break;
+            case 1:
+                address = userInput;
+                serviceStep++;
+                printQuestion("city");
+                break;
+            case 2:
+                city = userInput;
+                serviceStep++;
+                printQuestion("country");
+                break;
+            case 3:
+                country = userInput;
+                System.out.println("Address has been changed");
+                handler.changeCustomerAddress(customerId, address, city, country);
+                userServiceChoice = -1;
+                serviceStep = 0;
+        }
     }
 
     private void registerNewCustomer(CustomerHandler handler, String userInput) {
@@ -104,16 +145,16 @@ public class CustomerServicePoint {
         int userInput = 0;
         try {
             userInput = Integer.parseInt(readLine);
-            if (userInput < 1 || userInput > 2) {
+            if (userInput < 1 || userInput > 3) {
                 throw new IntegerOutOfRangeException();
             }
             userServiceChoice = userInput;
         } catch (NumberFormatException | IntegerOutOfRangeException ex) {
-            System.out.println("Please enter a 1 or a 2");
+            System.out.println("Please enter a 1 ,2 or 3");
         }
         if (userInput == 1) {
             printQuestion("first name");
-        }else if(userInput ==2){
+        }else if(userInput == 2 || userInput == 3){
             //imagine this is really a login with a password.
             printQuestion("email");
         }
